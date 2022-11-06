48098da8411e9d3c6236eacbb7b4d97d2d0f6e63
Dhruvit <pateldhruvit0712@gmail.com>
2022-11-06 19:45:47 +0530

    react website

--- src/App.js
+++ src/App.js
@@ -1,42 +1,55 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import React from "react";
 
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import FCFS from "./components/fcfs";
+import SJF from "./components/sjf";
+import PS from "./components/ps";
+import RR from "./components/rr";
+import FCFSS from "./components/FcfsSimulate";
+import SJFS from "./components/SjfSimulate";
+import RRS from "./components/rrSimulate";
+import PSS from "./components/psSimulate";
+import AboutLa from "./components/AboutLa";
+
+import { useMediaQuery } from "@chakra-ui/react";
+// import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  const [isLargerThan1280] = useMediaQuery("(min-width: 1280px)");
+
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
-  );
-}
+    <>
+      <Router>
+        {/* <Navbar /> */}
+        {isLargerThan1280 ? (
+          <Navbar Headsize="xl" iconSize={32} />
+        ) : (
+          <Navbar Headsize="md" iconSize={20} />
+        )}
+
+        <Routes>  
+          <Route path="/" element={<Home/>} />
+          <Route path="/CpuScheduling" element={<Home/>} />
+          <Route path="/Home" element={<Home/>} />
+          <Route path="/about" element={<AboutLa/>} />
+          <Route path="/fcfs" element={<FCFS/>} />
+          <Route path="/sjf" element={<SJF/>} />
+          <Route path="/rr" element={<RR/>} />
+          <Route path="/ps" element={<PS/>} />
+          <Route path="/fcfs/fcfsSimulate" element={<FCFSS/>} />
+          <Route path="/sjf/sjfSimulate" element={<SJFS/>} />
+          <Route path="/rr/rrSimulate" element={<RRS/>} />
+          <Route path="/ps/psSimulate" element={<PSS/>} />
+        </Routes>
+
+        {/* {isLargerThan1280 ? <Footer height="45%" /> : <Footer height="75%" />} */}
+        {/* <Footer /> */}
+      </Router>
+    </>
+  );            
+};
 
 export default App;
